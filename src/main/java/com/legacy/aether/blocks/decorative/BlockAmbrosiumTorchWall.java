833002b3fdfb5a2dc01ec11086cf58af55328af8
Daniel She <shekwancheung0528@gmail.com>
2019-03-08 16:15:51 +0800

    Update BlockAmbrosiumTorchWall.java

--- src/main/java/com/legacy/aether/blocks/decorative/BlockAmbrosiumTorchWall.java
+++ src/main/java/com/legacy/aether/blocks/decorative/BlockAmbrosiumTorchWall.java
@@ -1,5 +1,6 @@
 package com.legacy.aether.blocks.decorative;
 
+import com.legacy.aether.blocks.BlocksAether;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Material;
 import net.minecraft.block.WallTorchBlock;
@@ -10,7 +11,7 @@ public class BlockAmbrosiumTorchWall extends WallTorchBlock
 
 	public BlockAmbrosiumTorchWall()
 	{
-		super(FabricBlockSettings.of(Material.PART).collidable(false).breakByHand(true).ticksRandomly().lightLevel(1).sounds(BlockSoundGroup.WOOD).build());
+		super(FabricBlockSettings.of(Material.PART).collidable(false).breakByHand(true).ticksRandomly().lightLevel(1).sounds(BlockSoundGroup.WOOD).dropsLike(BlocksAether.ambrosium_torch).build());
 	}
 
 }
